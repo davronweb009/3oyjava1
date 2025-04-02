a3f81a93fffb6a3eccbdf31b8451de6b65593d51
davronweb009 <davronweb07@mail.ru>
2025-04-02 16:43:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    }
+});
